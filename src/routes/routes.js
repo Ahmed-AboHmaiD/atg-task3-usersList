8ecfaa7b0319fbad0162fb961fb15c277eb2e755
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-02-09 19:32:38 +0200

    Fix Route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import UserInfo from "../components/UsersInfo";
 
 export const router = createBrowserRouter([
   {
@@ -8,7 +7,7 @@ export const router = createBrowserRouter([
     element: <App />,
     children: [
       { index: true, element: <App /> },
-      { path: "/user/:id", element: <UserInfo /> },
+      { path: "/user/:id", element: <App /> },
     ],
   },
 ]);
